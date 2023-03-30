23687d9108c099e0e731977c160f72ac0529aa06
arthy-uthayasuhanthan <arthy.uthaya@gmail.com>
2023-03-30 09:41:28 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        debugShowCheckedModeBanner: false,
         home: const LoginPage());
   }
 }
